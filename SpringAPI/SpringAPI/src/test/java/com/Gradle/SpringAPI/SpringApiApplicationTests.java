a0aacdf0d3ed35c48491d1e738becc6e3589eb25
Darkness6917 <94816940+Darkness6917@users.noreply.github.com>
2023-01-16 12:59:50 -0800

    JavaWebServiceProject
    Project to create API endpoint.
    Running Gradle in Docker container.
    Will run unit tests against to test.
    Created on Start.Spring.io using gradle JDK19

---
+++ SpringAPI/SpringAPI/src/test/java/com/Gradle/SpringAPI/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gradle.SpringAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
